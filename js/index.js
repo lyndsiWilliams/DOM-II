4bb08f4b593f2606c832603448caee8515ebe08a
lyndsiWilliams <kcatgirl@gmail.com>
2019-10-29 13:30:16 -0500

    Created mouseover event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,41 @@
 // Your code goes here
+
+// mouseover
+const imgGrab = document.querySelectorAll('img');
+
+imgGrab.forEach(el => {
+    el.addEventListener("mouseenter", () => {
+        el.style.filter = "invert(100%)";
+    })
+
+    el.addEventListener("mouseleave", () => {
+        el.style.filter = "invert(0%)";
+    })
+})
+
+
+// keydown
+
+
+// wheel
+
+
+// drag / drop
+
+
+// load
+
+
+// focus
+
+
+// resize
+
+
+// scroll
+
+
+// select
+
+
+// dblclick
